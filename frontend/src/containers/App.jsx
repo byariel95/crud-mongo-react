769a0c28a796119af49c8870f3c8230c9ffb50a0
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-07-26 11:08:15 -0600

    structure creation

---
+++ frontend/src/containers/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Main from '../components/Main';
+import Navigation from '../components/Navigation';
+
+const App = () => {
+   
+    return(
+        <Main>
+            <Navigation/>
+        </Main>
+    )
+    
+};
+
+export default App;
