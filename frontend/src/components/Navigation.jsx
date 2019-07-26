44086e0eae0e88b4f9c4dc37880682aa414e6d5e
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-07-26 14:08:51 -0600

    create navigation

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
-    return (
-        <div>
-            navigation
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          React App
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link">Notes</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">New Notes</a>
+            </li>
+          </ul>
         </div>
-    )
-}
+      </div>
+    </nav>
+  );
+};
 
-export default Navigation
+export default Navigation;
